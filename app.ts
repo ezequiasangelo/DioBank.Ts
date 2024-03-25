ceb2cbf7a35e94597eb7be50daaf6161f5a5addf
unknown <ezequiasangelo>
2024-03-24 23:03:00 -0300

    Validate

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import { PeopleAccount } from './class/PeopleAccount'
 
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'ezequias', 10)
+console.log(peopleAccount)
 peopleAccount.deposit()
-
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-console.log(companyAccount.deposit())
+// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+// console.log(companyAccount.deposit())
